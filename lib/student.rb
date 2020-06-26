ed620e88bd229c3c3d856fbec8d40b991e4ec191
korey ko <koreykendrick1@gmail.com>
2020-06-26 17:40:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,7 +72,7 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-       SELECT COUNT(grade > 10) FROM students
+       SELECT COUNT(grade > 12) FROM students
     SQL
     DB[:conn].execute(sql)
     
